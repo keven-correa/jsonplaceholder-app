524f24f998a3417b11959362bf073f6703ea46d8
keven-correa <kturvi01@gmail.com>
2022-07-03 22:06:19 -0400

    models

---
+++ lib/models/address.dart
@@ -0,0 +1,35 @@
+
+
+import 'package:jsonplaceholder_app/models/geo.dart';
+
+class Address {
+  String? street;
+  String? suite;
+  String? city;
+  String? zipcode;
+  Geo? geo;
+
+  Address({
+    this.street,
+    this.suite,
+    this.city,
+    this.zipcode,
+    this.geo,
+  });
+
+  factory Address.fromJson(Map<String, dynamic> json) => Address(
+    street: json["street"],
+    suite: json["suite"],
+    city: json["city"],
+    zipcode: json["zipcode"],
+    geo: Geo.fromJson(json["geo"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "street": street,
+    "suite": suite,
+    "city": city,
+    "zipcode": zipcode,
+    "geo": geo?.toJson(),
+  };
+}
